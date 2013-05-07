bd07eea6dd344c433a351d2d2ace388fe8cc3f45
David Latham <kiwilathams@gmail.com>
2013-05-08 11:10:18 +1200

    fixed mpath_devlist.pl

--- mpath_devlist.pl
+++ mpath_devlist.pl
@@ -15,54 +15,53 @@
 ## License: GPL version 2
 
 # vim:expandtab:ts=4:softtabstop=4:autoindent
-
 use Term::ANSIColor;
 
- = 0;
- = 0;
+$count = 0;
+$pcount = 0;
 #print "PATH\tUID\t\t\t\t\tDM-ID\tSIZE\t\tACTIVE\tRESULT\n";
 printf ("%-16s%-40s%-10s%-10s%-10s%-10s\n", "PATH", "UID", "DM-ID", "SIZE", "ACTIVE", "RESULT");
 while(<STDIN>) {
-        my () = mpath_devlist/;
-        chomp();
-        if(  =~ /^[a-zA-Z]/ ) {
-                my(  ) = ;
+        my ($line) = $_;
+        chomp($line);
+        if( $line =~ /^[a-zA-Z]/ ) {
+                my( $id ) = $1;
                 /^(\w*) \((\w*)\)\s*(dm-\d*)/;
-                if(  > 0 ) {
-                         = " of 4";
-                        printf("%-10s",);
-                        if(  == 4) {
+                if( $count > 0 ) {
+                        $counttext = "$count of 4";
+                        printf("%-10s",$counttext);
+                        if( $count == 4) {
                                 print  color 'bold green'; printf("%-10s", "OK"); print color 'reset'; print "\n";
                         } else {
                                 print color 'bold red'; printf("%-10s", "PROBLEM"); print color 'reset'; print "\n";
                         }
-                         = 0;
+                        $count = 0;
                 }
-                my () = ;
-                my ()  = ;
-                my () = ;
-                printf( "%-16s%-40s%-10s", ,,);
-                 += 1;
+                my ($pname) = $1;
+                my ($puid)  = $2;
+                my ($pdmid) = $3;
+                printf( "%-16s%-40s%-10s", $pname,$puid,$pdmid);
+                $pcount += 1;
         }
-        if (  =~ m/^\[(.*?)\]/ ) {
-                printf("%-10s", );
+        if ( $line =~ m/^\[(.*?)\]/ ) {
+                printf("%-10s", $1);
         }
-        if (  =~ /^\s\_.*active\]\[ready/ ) {
-                 += 1;
+        if ( $line =~ /^\s\\_.*active\]\[ready/ ) {
+                $count += 1;
         }
 }
-if(  > 0 ) {
-         = " of 4";
-        printf("%-10s", );
-        if(  == 4) {
+if( $count > 0 ) {
+        $counttext = "$count of 4";
+        printf("%-10s", $counttext);
+        if( $count == 4) {
                 print  color 'bold green'; printf("%-10s","OK"); print color 'reset'; print "\n";
         } else {
                 print color 'bold red'; printf("%-10s","PROBLEM"); print color 'reset'; print "\n";
         }
 
-         = 0;
+        $count = 0;
 }
 print color 'reset';
 print "\n";
-print "Found  paths\n";
+print "Found $pcount paths\n";
 exit 0;
